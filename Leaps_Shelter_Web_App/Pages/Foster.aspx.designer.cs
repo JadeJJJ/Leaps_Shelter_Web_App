8a8ddd3e80d8b6427767e83c421b0ccd763aca01
jmvn26 <93352666+jmvn26@users.noreply.github.com>
2022-10-02 15:11:29 +0200

    Updated page headers

--- Leaps_Shelter_Web_App/Pages/Foster.aspx.designer.cs
+++ Leaps_Shelter_Web_App/Pages/Foster.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace Leaps_Shelter_Web_App.Pages
 
     public partial class Foster
     {
+
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
