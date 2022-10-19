4ee1fe5de77810df4fde22e8a1b4cde7278311c0
James Marx <20102645@vcconnect.co.za>
2022-10-19 14:39:15 +0200

    opens in new

--- Leaps_Shelter_Web_App/Pages/ContactUs.aspx.cs
+++ Leaps_Shelter_Web_App/Pages/ContactUs.aspx.cs
@@ -21,30 +21,32 @@ namespace Leaps_Shelter_Web_App.Pages
 
         public void EmailSender()
         {
-            string userEmailCredentials2 = "USERS CONTACT DETAILS: ";
-            string userEmailCredentials = userEmailTxt.Text;
-            string to = emailToTxt.Text; //To address    
-            string from = "contactingus4leaps@gmail.com"; //From address    
-            MailMessage message = new MailMessage(from, to);
-
-            string mailbody = bodyTxt.Text;
-
-            message.Subject = subjectTxt.Text;
-            message.Body = mailbody + "\n" + userEmailCredentials2 + userEmailCredentials;
-            message.BodyEncoding = Encoding.UTF8;
-            message.IsBodyHtml = true;
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp
-            client.Credentials = new System.Net.NetworkCredential("username", "password");
-            System.Net.NetworkCredential basicCredential1 = new
-            System.Net.NetworkCredential("contactingus4leaps@gmail.com", "yyjjhgxkwnsbdihx");
-            client.EnableSsl = true;
-            client.UseDefaultCredentials = false;
-            client.Credentials = basicCredential1;
-
             
                 try
                 {
-                    client.Send(message);
+
+                string userEmailCredentials2 = "USERS CONTACT DETAILS: ";
+                string userEmailCredentials = userEmailTxt.Text;
+                string to = emailToTxt.Text; //To address    
+                string from = "contactingus4leaps@gmail.com"; //From address    
+                MailMessage message = new MailMessage(from, to);
+
+                string mailbody = bodyTxt.Text;
+
+                message.Subject = subjectTxt.Text;
+                message.Body = mailbody + "\n" + userEmailCredentials2 + userEmailCredentials;
+                message.BodyEncoding = Encoding.UTF8;
+                message.IsBodyHtml = true;
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp
+                client.Credentials = new System.Net.NetworkCredential("username", "password");
+                System.Net.NetworkCredential basicCredential1 = new
+                System.Net.NetworkCredential("contactingus4leaps@gmail.com", "yyjjhgxkwnsbdihx");
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = basicCredential1;
+
+                client.Send(message);
+
                 }
 
                 catch (Exception ex)
