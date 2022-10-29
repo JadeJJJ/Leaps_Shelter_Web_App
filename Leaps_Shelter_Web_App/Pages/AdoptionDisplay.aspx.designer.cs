664d668daebcc6c46cb4c7a6336bab2577311ccd
James Marx <20102645@vcconnect.co.za>
2022-10-29 12:12:33 +0200

    bill is a beast

--- Leaps_Shelter_Web_App/Pages/AdoptionDisplay.aspx.designer.cs
+++ Leaps_Shelter_Web_App/Pages/AdoptionDisplay.aspx.designer.cs
@@ -24,21 +24,66 @@ namespace Leaps_Shelter_Web_App.Pages
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
-        /// AdoptDogDG control.
+        /// DropDownList1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DataGrid AdoptDogDG;
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
 
         /// <summary>
-        /// GridView1 control.
+        /// DropDownList2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
+
+        /// <summary>
+        /// DropDownList3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
+
+        /// <summary>
+        /// DropDownList4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList4;
+
+        /// <summary>
+        /// DropDownList5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList5;
+
+        /// <summary>
+        /// adoptDisplayDL control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList adoptDisplayDL;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
--- Leaps_Shelter_Web_App/Pages/addAnimalsAdoption.aspx.cs
+++ Leaps_Shelter_Web_App/Pages/addAnimalsAdoption.aspx.cs
@@ -1,17 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using System.IO;
 
 namespace Leaps_Shelter_Web_App.Pages
 {
     public partial class addAnimalsAdoption : System.Web.UI.Page
     {
+
+        //string LeapsShelterCS = ConfigurationManager.ConnectionStrings["LeapsCS"].ConnectionString; // String referencing the connection string in the Web.config file
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            GVbind();
+            ClearEntries();
+           // 
 
+        }
+
+        protected void GVbind()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\LeapsShelterDB.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * from leapsANIMAL", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows == true)
+            {
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+            }
+        }
+
+        public void ClearEntries()
+        {
+
+
+            animalNameTB.Text = "";
+            animalAgeTB.Text = "";
+            animalTypeDDL.ClearSelection();
+            genderDDL.ClearSelection();
+            sterilizationDDL.ClearSelection();
+            vaccinationDDL.ClearSelection();
+            breedTB.Text = "";
+            addAnimalPhoto.Attributes.Clear();
+            descriptionTB.Text = "";
+
+        }
+
+
+        protected void addAnimalBTN_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con1 = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\LeapsShelterDB.mdf;Integrated Security=True");
+
+            try
+            {
+                string extension = Path.GetExtension(addAnimalPhoto.FileName);
+                if (extension == ".jpg" || extension == ".jpeg" || extension == ".png")
+                {
+
+                    if (addAnimalPhoto.PostedFile.ContentLength <= 500000)
+                    {
+
+                        if (addAnimalPhoto.HasFiles || animalNameTB.Text == String.Empty || animalTypeDDL.SelectedItem.Text.ToString() == String.Empty || breedTB.Text == String.Empty || animalAgeTB.Text == String.Empty || genderDDL.SelectedItem.Text.ToString() == String.Empty || descriptionTB.Text == String.Empty || sterilizationDDL.SelectedItem.Text.ToString() == String.Empty || vaccinationDDL.SelectedItem.Text.ToString() == String.Empty)
+                        {
+
+
+                            string fname = Path.GetFileName(addAnimalPhoto.FileName);
+                            addAnimalPhoto.SaveAs(Server.MapPath("~/AdoptionPhotos/") + fname);
+                            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\LeapsShelterDB.mdf;Integrated Security=True");
+                            con.Open();
+                            SqlCommand cmd = new SqlCommand("insert into leapsANIMAL values('" + addAnimalPhoto.FileName + "','" + animalNameTB.Text + "','" + animalTypeDDL.SelectedItem.Text.ToString() + "','" + breedTB.Text + "','" + animalAgeTB.Text + "','" + genderDDL.SelectedItem.Text.ToString() + "','" + descriptionTB.Text + "','" + sterilizationDDL.SelectedItem.Text.ToString() + "','" + vaccinationDDL.SelectedItem.Text.ToString() + "')", con);
+                            //int t =
+                                cmd.ExecuteNonQuery();
+                            // if (t > 0)
+                            // {
+                            
+                                //Clear();
+                                //this.GridView1.DataBind();
+                                ClearEntries();
+                            
+                                GVbind();
+                           // }
+                        }
+                        else
+                        {
+                            Label1.Text = "Please fill in all the fields!";
+                            Label1.ForeColor = System.Drawing.Color.Red;
+                        }
+                    }
+
+                    else
+                    {
+                        Label1.Text = "The file needs to be smaller than 5mb!";
+                        Label1.ForeColor = System.Drawing.Color.Red;
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = "only jpg,jpeg or png Files are accepted!!" + ex.Message;
+            }
+
+            Response.Redirect("addAnimalsAdoption.aspx");
+            Response.Write("<script>alert('Animal has been uploaded to the adoption page!')</script>");
         }
     }
 }
