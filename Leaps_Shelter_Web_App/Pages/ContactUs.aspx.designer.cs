b85724b3b0fe83658a399f67cfaba644f2ff17fd
James Marx <20102645@vcconnect.co.za>
2022-10-30 23:17:22 +0200

    awe awe

--- Leaps_Shelter_Web_App/Pages/ContactUs.aspx.designer.cs
+++ Leaps_Shelter_Web_App/Pages/ContactUs.aspx.designer.cs
@@ -33,13 +33,13 @@ namespace Leaps_Shelter_Web_App.Pages
         protected global::System.Web.UI.WebControls.Panel Panel1;
 
         /// <summary>
-        /// Label3 control.
+        /// Label1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
+        protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
         /// userEmailTxt control.
