ba73808cea5a8686e41141f46511ae363f4e9d72
JayDWrld <101358812+JayDWrld@users.noreply.github.com>
2022-10-30 18:11:07 +0200

    UI

--- Leaps_Shelter_Web_App/Pages/Adopt.aspx.designer.cs
+++ Leaps_Shelter_Web_App/Pages/Adopt.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace Leaps_Shelter_Web_App.Pages
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
+        /// HyperLink3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink3;
+
+        /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+
+        /// <summary>
         /// categoryDropdown control.
         /// </summary>
         /// <remarks>
